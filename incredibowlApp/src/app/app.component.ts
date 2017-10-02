caee4487e4cd5eec7fa2f8fb18daa0145069fd72
Ryan Baarz <rbaarz@adobe.com>
2017-10-02 00:13:27 -0600

    animation and click interaction is in

--- incredibowlApp/src/app/app.component.ts
+++ incredibowlApp/src/app/app.component.ts
@@ -1,31 +1,78 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { ScoreboxComponent } from './components/scorebox/scorebox.component';
 import { LaneComponent } from './components/lane/lane.component';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   @ViewChild(ScoreboxComponent)
   scorebox: ScoreboxComponent;
 
   @ViewChild(LaneComponent)
   lane: LaneComponent;
 
+  showClickToBowl = true;
+  ballWidth = 320;
+  ballSpeed = 1000;
+
   constructor() {
 
   }
 
+  ngAfterViewInit() {
+    this.animateBall( $('.bowling-ball') , this.ballSpeed);
+  }
+
+  animateBall(targetElement, speed) {
+    let self = this;
+    targetElement.animate({ marginLeft: '+=' + self.ballWidth + 'px'},
+      {
+        duration: speed,
+        complete: function ()
+        {
+          targetElement.animate({ marginLeft: '-=' + self.ballWidth + 'px' },
+            {
+              duration: speed,
+              complete: function ()
+              {
+                self.animateBall(targetElement, speed);
+              }
+            });
+        }
+      });
+  };
+
   generateRoll() {
-    let randIn = Math.floor(Math.random() * 10) + 1,
+    let randIn = this.getLuckyNumber(),
         pinsDown = this.lane.knockPinsDown(randIn),
         frameDone = this.scorebox.recordBowl(pinsDown);
-        if (frameDone) {
-          setTimeout(() => {this.lane.setPins();}, 1000); // give the user a chance to see the pins knocked down
 
-        }
+    this.toggleClickToBowl(null);
+    if (frameDone) {
+      setTimeout(() => {
+        this.lane.setPins();
+      }, 1000); // give the user a chance to see the pins knocked down
+    }
+  }
+
+  getLuckyNumber() {
+    let seed = Number($('.bowling-ball').css('marginLeft').replace('px', '')),
+        ratio = seed / this.ballWidth;
+    return Math.ceil(ratio * 10);
+  }
+
+  toggleClickToBowl(event) {
+    if (event) {
+      event.stopPropagation();
+    }
+
+    this.showClickToBowl = !this.showClickToBowl;
+
   }
 }
 
